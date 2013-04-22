369a1bdf152527b2081e5e34885850996773e62b
Leonardo Souza <leonardo@santopixel.com.br>
2013-04-22 09:31:59 -0300

    Second Commit

---
+++ default.php
@@ -0,0 +1,288 @@
+<?php header( 'Content-Type: text/html; charset=utf-8' ) ?><!doctype html>
+<html class="no-js" lang="pt-br" dir="ltr">
+	<head>
+		<meta name="viewport" content="width=device-width, initial-scale=1,maximum-scale=1, user-scalable=0" />
+		<meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1" />
+		<meta name="author" content="SantoPixel - Design &amp; Code" />
+		<meta name="URL" content="http://www.santopixel.com.br" />
+		<meta name="robots" content="index, follow" />
+
+		<title>SantoPixel - Design &amp; Code</title>
+		<meta name="description" content="Desenvolvimento Front-end e Design para todas as plataformas" />
+		<meta name="keywords" content="design, front-end, branding, web, site, internet, desenvolvimento" />
+		<!-- google+ -->
+		<meta itemprop="name" content="SantoPixel" />
+		<meta itemprop="description" content="Desenvolvimento Front-end e Design para todas as plataformas" />
+		<meta itemprop="image" content="http://www.pixelsanto.com/img/external/santopixel-logo-googleplus.png" />
+		<!-- facebook -->
+		<meta property="fb:app_id" content="483733905015356" />
+		<meta property="og:type" content="company" />
+		<meta property="og:title" content="SantoPixel" />
+		<meta property="og:image" content="http://www.pixelsanto.com/img/external/santopixel-logo-facebook.png" />
+		<meta property="og:description" content="Desenvolvimento Front-end e Design para todas as plataformas" />
+		<meta property="og:url" content="http://www.santopixel.com.br" />
+
+		<link rel="stylesheet" href="//pixelsanto.com/css/santopixel.min.css" media="all" />
+		<link href="//fonts.googleapis.com/css?family=Bitter:400,700,400italic" rel="stylesheet" type="text/css" />
+		<link href="//fonts.googleapis.com/css?family=Merriweather:400,300,700,900" rel="stylesheet" type="text/css" />
+
+		<link rel="shortcut icon" href="//pixelsanto.com/favicon.ico" type="image/x-icon" />
+		<link rel="icon" href="//pixelsanto.com/favicon.ico" type="image/x-icon" />
+		<link rel="apple-touch-icon" href="//pixelsanto.com/img/external/apple-touch-icon-57x57.png" />
+		<link rel="apple-touch-icon" sizes="72x72" href="//pixelsanto.com/img/external/apple-touch-icon-72x72.png" />
+		<link rel="apple-touch-icon" sizes="114x114" href="//pixelsanto.com/img/external/apple-touch-icon-114x114.png" />
+		<link rel="apple-touch-icon" sizes="144x144" href="//pixelsanto.com/img/external/apple-touch-icon-144x144.png" />
+
+		<script src="//pixelsanto.com/js/libs/head.min.js"></script>
+	</head>
+
+	<body>
+		<div id="persistent-menu" class="persistent-menu">
+			<div class="container">
+				<div class="menu-logo ir">SantoPixel - Design &amp; Code</div>
+				<div class="btn-menu ir" id="bt-expand-mobile"><div class="btn-menu-int">expandir menu</div></div>
+
+				<nav class="menu" id="ctn-expand-mobile">
+					<ul class="nav-list">
+						<li class="nav-item"><a class="nav-link" href="#top" title="home">HOME</a></li>
+						<li class="nav-item"><a class="nav-link" href="#o-que-fazemos" title="o que fazemos">O QUE FAZEMOS</a></li>
+						<li class="nav-item"><a class="nav-link" href="#como-fazemos" title="como fazemos">COMO FAZEMOS</a></li>
+						<li class="nav-item"><a class="nav-link" href="#projetos" title="projetos">PROJETOS</a></li>
+						<li class="nav-item"><a class="nav-link" href="#quem-somos" title="quem somos">QUEM SOMOS</a></li>
+						<li class="nav-item"><a class="nav-link" href="#contato" title="contato">CONTATO</a></li>
+					</ul>
+				</nav>
+			</div>
+		</div>
+
+		<header id="top">
+			<div class="container">
+				<div class="box-socials">
+					<?php include 'socials.php' ?>
+				</div>
+
+				<div class="box-right">
+					<div class="logo">
+						<img src="//pixelsanto.com/img/santopixel-logo-scaled.png" height="496" width="304" alt="SantoPixel - Design &amp; Code" />
+					</div>
+				</div>
+
+				<div class="box-left">
+					<section class="featured">
+						<h1 class="title title-a">DESENVOLVIMENTO FRONT-END E DESIGN PARA TODAS AS PLATAFORMAS</h1>
+						<p class="text text-a">Trazemos marcas para a vida. Usamos nossos conhecimentos em design e desenvolvimento web para ajudar empresas a crescer e melhorar a sua reputação.</p>
+					</section>
+
+					<nav class="menu" id="top-menu">
+						<ul class="nav-list">
+							<li class="nav-item current"><a class="nav-link" href="#top" title="home">HOME</a></li>
+							<li class="nav-item"><a class="nav-link" href="#o-que-fazemos" title="o que fazemos">O QUE FAZEMOS</a></li>
+							<li class="nav-item"><a class="nav-link" href="#como-fazemos" title="como fazemos">COMO FAZEMOS</a></li>
+							<li class="nav-item"><a class="nav-link" href="#projetos" title="projetos">PROJETOS</a></li>
+							<li class="nav-item"><a class="nav-link" href="#quem-somos" title="quem somos">QUEM SOMOS</a></li>
+							<li class="nav-item"><a class="nav-link" href="#contato" title="contato">CONTATO</a></li>
+						</ul>
+					</nav>
+				</div>
+			</div>
+		</header>
+
+		<section class="section whatwedo" id="o-que-fazemos">
+			<div class="container">
+				<div class="line mar80">
+					<h2 class="tophat tophat-a">O QUE FAZEMOS</h2>	
+				</div>
+
+				<div class="line">
+					<div class="item-desc">
+						<i class="sprite-icons-a ico-a2">&nbsp;</i>
+						<h3 class="title title-c">Design para Web</h3>
+						<p class="text text-b">Arquitetura da Informação, UI &amp; UX e Design Gráfico.</p>
+						<p class="text text-b">Projetamos interfaces intuitivas e de fácil navegação, proporcionando uma melhor experiência para o usuário.</p>
+					</div>
+
+					<div class="item-desc">
+						<i class="sprite-icons-a ico-a3">&nbsp;</i>
+						<h3 class="title title-c">Front-End</h3>
+						<p class="text text-b">HTML5, CSS3, JavaScript, WordPress e SEO.</p>
+						<p class="text text-b">Desenvolvemos sites acessíveis, responsivos e de alto desempenho. Feitos com código adaptável e escalável.</p>
+					</div>
+
+					<div class="item-desc">
+						<i class="sprite-icons-a ico-a1">&nbsp;</i>
+						<h3 class="title title-c">Design de Marcas</h3>
+						<p class="text text-b">Concepção, redesign e normatização de marcas e sistemas de identidade visual.</p>
+						<p class="text text-b">Traduzimos valores e estratégias do cliente em expressões visuais para fortalecer sua imagem corporativa ou de seu produto.</p>
+					</div>
+				</div>
+			</div>
+		</section>
+
+		<section class="section howwedo" id="como-fazemos">
+			<div class="container">
+				<div class="line mar80">
+					<h2 class="tophat tophat-a">COMO FAZEMOS</h2>	
+				</div>
+
+				<div class="line">
+					<p class="text text-c">Trabalhamos remotamente de maneira dedicada, integrada e colaborativa.</p>
+					<p class="text text-c">Nossa responsabilidade viabiliza a liberdade de escolher como e onde trabalhar, nos provendo a atmosfera ideal para entregarmos um produto de alta qualidade.</p>
+				</div>
+			</div>
+		</section>
+
+		<section class="section projects" id="projetos">
+			<div class="container">
+				<div class="line mar80">
+					<h2 class="tophat tophat-b">PROJETOS</h2>	
+				</div>
+			</div>
+
+			<img src="//pixelsanto.com/img/projetos-kzuka.jpg" height="312" width="1440" alt="Novo site do Kzuka do Grupo RBS" class="img-100" />
+
+			<div class="container">
+				<div class="box-left">
+					<h3 class="title title-b">Novo Kzuka</h3>
+					<p class="text text-d">Projeto do novo site do Kzuka, que tinha como alguns dos grandes desafios levar a identidade visual da marca do off-line para o on-line, integrando o visual e conteúdo de todas as mídias da empresa.</p>
+					<p class="text text-d">Foram 11 seções desenvolvidas, todas com visual exclusivo. Galeria de fotos, notícias, blogs e ampla integração com redes sociais são as funcionalidades em destaque do produto.</p>
+				</div>
+			</div>
+		</section>
+
+		<section class="section whoweare" id="quem-somos">
+			<div class="container">
+				<div class="line mar80">
+					<h2 class="tophat tophat-a">QUEM SOMOS</h2>	
+				</div>
+
+				<div class="line">
+					<div class="box-left">
+						<p class="text text-b">Somos um time multidisciplinar, amigos e colegas há mais de 6 anos e sócios desde 2012.</p>
+						<p class="text text-b">A experiência em grandes empresas, participando de projetos desafiadores, nos motivaram a encarar o mercado digital como empreendedores.</p>
+						<p class="text text-b">Queremos ser uma empresa de sucesso, sem perder nossos valores essenciais: fazer o certo sempre, acreditar nas pessoas, melhorar o mundo onde vivemos.</p>
+					</div>
+
+					<div class="box-right">
+						<div class="member felipe-barreto" itemscope itemtype="http://data-vocabulary.org/Person">
+							<div class="member-photo">
+								<img src="//pixelsanto.com/img/felipe-barreto-designer.jpg" class="photo" alt="Felipe Barreto - Designer da SantoPixel" width="136" height="136" />
+							</div>
+
+							<h3 class="title title-d" itemprop="name">Felipe Barreto</h3>
+						 	<h4 class="subtitle subtitle-a" itemprop="role">Designer</h4>
+							<a href="mailto: felipe@santopixel.com.br" class="member-mail" title="enviar email para felipe@santopixel.com.br">felipe@santopixel.com.br</a>
+							<a href="http://br.linkedin.com/in/felipebarretoribeiro" class="member-linkedin" title="LinkedIn Felipe Barreto" target="_blank" itemprop="url">LinkedIn</a>
+
+							<span itemprop="address" itemscope itemtype="http://data-vocabulary.org/Address" style="color: #221e1f;">
+						    	<span itemprop="locality">Porto Alegre</span>, <span itemprop="region">Rio Grande do Sul</span>
+						    </span>
+						</div>
+
+						<div class="member fernando-tessmann" itemscope itemtype="http://data-vocabulary.org/Person">
+							<div class="member-photo">
+								<img src="//pixelsanto.com/img/fernando-tessmann-developer.jpg" class="photo" alt="Fernando Tessmann - Developer da SantoPixel" width="136" height="136" />
+							</div>
+
+							<h3 class="title title-d" itemprop="name">Fernando Tessmann</h3>
+						 	<h4 class="subtitle subtitle-a" itemprop="role">Developer</h4>
+							<a href="mailto: fernando@santopixel.com.br" class="member-mail" title="enviar email para fernando@santopixel.com.br">fernando@santopixel.com.br</a>
+							<a href="http://br.linkedin.com/in/nandotess" class="member-linkedin" title="LinkedIn Fernando Tessmann" target="_blank" itemprop="url">LinkedIn</a>
+
+							<span itemprop="address" itemscope itemtype="http://data-vocabulary.org/Address" style="color: #221e1f;">
+						    	<span itemprop="locality">Porto Alegre</span>, <span itemprop="region">Rio Grande do Sul</span>
+						    </span>
+						</div>
+
+						<div class="member leonardo-souza" itemscope itemtype="http://data-vocabulary.org/Person">
+							<div class="member-photo">
+								<img itemprop="name" src="//pixelsanto.com/img/leonardo-souza-developer.jpg" class="photo" alt="Leonardo Souza - Developer da SantoPixel" width="136" height="136" />
+							</div>
+
+							<h3 class="title title-d" itemprop="name">Leonardo Souza</h3>
+						 	<h4 class="subtitle subtitle-a" itemprop="role">Developer</h4>
+							<a href="mailto: leonardo@santopixel.com.br" class="member-mail" title="enviar email para leonardo@santopixel.com.br">leonardo@santopixel.com.br</a>
+							<a href="http://br.linkedin.com/in/leonardosouzadasilva" class="member-linkedin" title="LinkedIn Leonardo Souza" target="_blank" itemprop="url">LinkedIn</a>
+							
+							<span itemprop="address" itemscope itemtype="http://data-vocabulary.org/Address" style="color: #221e1f;">
+						    	<span itemprop="locality">Porto Alegre</span>, <span itemprop="region">Rio Grande do Sul</span>
+						    </span>
+						</div>
+					</div>
+				</div>
+			</div>
+		</section>
+
+		<section class="section contact" id="contato">
+			<div class="container">
+				<div class="line mar80">
+					<h2 class="tophat tophat-b">CONTATO</h2>	
+				</div>
+
+				<div class="line">
+					<div class="box-right">
+						<form action="php/contact-submit.php" method="POST" id="contact-form" name="contact-form" class="form">
+							<fieldset>
+								<legend>Entre em contato com a SantoPixel</legend>
+
+								<ul class="list-form">
+									<li class="item-form">
+										<input type="text" class="input-text validate" id="txt-name" name="txt-name" placeholder="Nome" accesskey="n" />
+										<div class="form-feedback"></div>
+									</li>
+
+									<li class="item-form">
+										<input type="text" class="input-text validate type-email" id="txt-email" name="txt-email" placeholder="Email" accesskey="e" />
+										<div class="form-feedback"></div>
+									</li>
+
+									<li class="item-form">
+										<textarea class="textarea validate" id="txt-msg" name="txt-msg" placeholder="Mensagem" accesskey="m"></textarea>
+										<div class="form-feedback"></div>
+									</li>
+
+									<li class="item-form button">
+										<input type="submit" class="btn" id="btn-send" name="btn-send" value="ENVIAR" />
+									</li>
+								</ul>
+							</fieldset>
+						</form>
+
+						<div class="extra-infos">
+							<div class="extra-info">
+								<i class="sprite-icons-b ico-b3"> </i>
+								<a href="mailto: contato@santopixel.com.br" class="link">contato@santopixel.com.br</a>
+							</div>
+						</div>
+					</div>
+				</div>
+			</div>
+		</section>
+
+		<footer class="footer">
+			<div class="container">
+				<p class="copyright">&copy; 2013 SantoPixel</p>
+
+				<div class="box-socials">
+					<?php include 'socials.php' ?>
+				</div>
+
+				<div class="footer-logo ir">SantoPixel - Design &amp; Code</div>
+			</div>
+		</footer>
+
+		<script type="text/javascript">
+			head.js("//pixelsanto.com/js/libs/jquery-1.9.1.min.js", "//pixelsanto.com/js/SantoPixel.min.js", function() {
+		   		var appSiteSantoPixel = new SiteSantoPixel.app();
+				appSiteSantoPixel.init();
+
+				head.js("http://www.google-analytics.com/ga.js", function() {
+					var _gaq = _gaq || [];
+					console.log(_gaq);
+					_gaq.push(['_setAccount', 'UA-38961120-1']);
+					_gaq.push(['_trackPageview']);
+				});
+			});
+		</script>
+		
+	</body>
+</html>
